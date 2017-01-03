a3a23a2e0f9e95d270bb60456cd4e23b885d44a3
Samuel Barabas <samuel@owee.de>
2017-01-03 23:43:23 +0100

    Removed default blogroll and added our Github account

--- pelicanconf.py
+++ pelicanconf.py
@@ -21,14 +21,13 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+#LINKS = (('Pelican', 'http://getpelican.com/'),
+#         ('Python.org', 'http://python.org/'),
+#         ('Jinja2', 'http://jinja.pocoo.org/'),
+#         ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('Github', 'https://github.com/Reaktor23'),)
 
 DEFAULT_PAGINATION = False
 

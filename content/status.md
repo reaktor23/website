51f8a370c320956ee4d02df6c60eb2500d346444
bouni <bouni@owee.de>
2018-06-26 16:05:39 +0200

    implemented first version of Reaktor Status display

---
+++ content/status.md
@@ -0,0 +1,10 @@
+---
+title: Reaktor 23 Space Status
+draft: false
+---
+
+Here you can find mor detailed infos about the state of our Hackerspace!
+We try to comply with the [SpaceAPI](http://spaceapi.net/) as good as possible :-)
+
+<div id="apidteails"></div>
+
---
+++ themes/reaktor23/static/js/reaktorstatus.js
@@ -0,0 +1,41 @@
+// get JSON according to the SpaceAPI
+var getstatus = function() {
+  $.getJSON("https://r23.mooo.com/spaceapi", function(data) {
+    $("#reaktorstatus > #message").removeClass();
+    var timeout = new Date().valueOf() - data.state.lastchange > 120;
+    if(data.state.open == "open" && !timeout) {
+      $("#reaktorstatus > #message").addClass("text-success");
+      $("#reaktorstatus > #message").html(data.state.message);
+    }
+    else if(data.state.open == "closed" && !timeout) {
+      $("#reaktorstatus > #message").addClass("text-danger");
+      $("#reaktorstatus > #message").html(data.state.message);
+    }
+    else {
+      $("#reaktorstatus > #message").addClass("text-warning");
+      $("#reaktorstatus > #message").html("Not sure, better contcat us first!");
+    }
+    if($("#apidteails")) {
+      $("#apidteails").html("");
+      printdetails(data);
+    }
+  });
+}
+
+var printdetails = function(data) {
+  $("#apidteails").append("<div>Last refresh</div>")
+  $("#apidteails").append("<div>"+new Date(data.state.lastchange)+"</div>")
+  $("#apidteails").append("<br/><div>Temperatures</div>")
+  data.sensors.temperatures.forEach(function(e) {
+    $("#apidteails").append("<div>"+e.name+": "+e.value+" "+e.unit+"</div>")
+  })
+  $("#apidteails").append("<br/><div>Power circuits</div>")
+  data.sensors.power_circuits.forEach(function(e) {
+    $("#apidteails").append("<div>"+e.name+": "+e.value+" "+e.unit+"</div>")
+  })
+}
+
+// Call function on page load
+getstatus();
+// Refresh status every 10 seconds
+window.setInterval(getstatus, 10000);
